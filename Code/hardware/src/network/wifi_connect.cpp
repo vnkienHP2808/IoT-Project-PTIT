526365608bf0aff7a36a82e9e2a5b5853f9fe808
hangrey19 <huongn0903@gmail.com>
2025-11-11 10:19:27 +0700

    Update comment

--- Code/hardware/src/network/wifi_connect.cpp
+++ Code/hardware/src/network/wifi_connect.cpp
@@ -1,20 +1,20 @@
-#include <WiFi.h>
+#include <WiFi.h>       // Thư viện wifi có sẵn trong core của ESP32, build thành công nên không cần để ý đến lỗi
 #include "config.h"
 
 void wifi_connect_init() {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    WiFi.mode(WIFI_STA);                        // Đặt ESP32 làm Station mode => thiết bị kết nối vào Wifi có sẵn
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);       // Bắt đầu kết nối đến mạng Wifi đã cấu hình trong config.h
     Serial.print("Connecting WiFi");
-    unsigned long start = millis();
+    unsigned long start = millis();             // Lưu thời điểm bắt đầu (đếm thời gian từ khi khởi động)
 
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {     // Kiểm tra trạng thái kết nối
         Serial.print(".");
         delay(500);
-        if (millis() - start > 20000)   break;
+        if (millis() - start > 20000)   break;  // Sau 20s nếu chưa kết nối được thì break để tránh treo
     }
 
     if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("\nWiFi connected: " + WiFi.localIP().toString());
+        Serial.println("\nWiFi connected: " + WiFi.localIP().toString());       // Kết nối thành công thì in ra địa chỉ IP mà ESP32 nhận được
     }
     else {
         Serial.println("\nWiFi connect failed");
@@ -22,5 +22,5 @@ void wifi_connect_init() {
 }
 
 bool wifi_is_connected() {
-    return WiFi.status() == WL_CONNECTED;
+    return WiFi.status() == WL_CONNECTED;       // Hàm thông báo trạng thái kết nối Wifi
 }
