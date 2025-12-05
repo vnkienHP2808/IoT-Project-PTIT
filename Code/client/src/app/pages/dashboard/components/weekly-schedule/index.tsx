92d2d963912651494d919e527a41a8bbcc815fcf
quanglam04 <trinhquanglam2k4@gmail.com>
2025-12-05 16:26:44 +0700

    update Dashboard

--- Code/client/src/app/pages/dashboard/components/weekly-schedule/index.tsx
+++ Code/client/src/app/pages/dashboard/components/weekly-schedule/index.tsx
@@ -1,8 +1,10 @@
+import { getDateFormat } from '@/shared/utils/date.util'
+
 const WeeklySchedule = () => {
   return (
     <div className='flex h-full flex-col rounded-3xl border-2 border-gray-800 bg-white p-6 shadow-lg'>
-      <h2 className='mb-2 text-2xl font-bold'>Lịch tưới hàng tuần</h2>
-      <div className='mb-6 text-gray-600'>10/11 - 16/11</div>
+      <h2 className='mb-2 text-2xl font-bold'>Lịch tưới ngày hôm nay</h2>
+      <div className='mb-6 text-gray-600'>{getDateFormat({ onlyDate: false })}</div>
 
       <div className='flex flex-1 items-center justify-center'>
         <p className='text-center text-gray-400 italic'>// Hiển thị lịch tưới đang Calendar</p>
